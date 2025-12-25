df702c25a10a990651bc96e6f38f0d48ef20542d
mojelumi <179169502+mojelumi0@users.noreply.github.com>
2025-12-25 23:33:43 +0100

    Update keyboard.h

--- main/hal/keyboard/keyboard.h
+++ main/hal/keyboard/keyboard.h
@@ -1,192 +1,194 @@
+#ifdef ARDUINO_M5STACK_CARDPUTER
 /**
  * @file keyboard.h
  * @author Forairaaaaa
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2023-09-22
- * 
+ *
  * @copyright Copyright (c) 2023
- * 
+ *
  */
 #pragma once
 #include <iostream>
 #include <vector>
-#include "keymap.h"
+#include "Arduino.h"
 
-
-namespace KEYBOARD 
+struct Chart_t
 {
-    struct Chart_t 
+    uint8_t value;
+    uint8_t x_1;
+    uint8_t x_2;
+};
+
+struct Point2D_t
+{
+    int x;
+    int y;
+};
+
+const std::vector<int> output_list = {8, 9, 11};
+const std::vector<int> input_list = {13, 15, 3, 4, 5, 6, 7};
+
+const Chart_t X_map_chart[7] = {{1, 0, 1}, {2, 2, 3}, {4, 4, 5}, {8, 6, 7}, {16, 8, 9}, {32, 10, 11}, {64, 12, 13}};
+
+struct KeyValue_t
+{
+    const char value_first;
+    const char value_second;
+};
+
+const KeyValue_t _key_value_map[4][14] = {{{'`', '~'},
+                                           {'1', '!'},
+                                           {'2', '@'},
+                                           {'3', '#'},
+                                           {'4', '$'},
+                                           {'5', '%'},
+                                           {'6', '^'},
+                                           {'7', '&'},
+                                           {'8', '*'},
+                                           {'9', '('},
+                                           {'0', ')'},
+                                           {'-', '_'},
+                                           {'=', '+'},
+                                           {KEY_BACKSPACE, KEY_BACKSPACE}},
+                                          {{KEY_TAB, KEY_TAB},
+                                           {'q', 'Q'},
+                                           {'w', 'W'},
+                                           {'e', 'E'},
+                                           {'r', 'R'},
+                                           {'t', 'T'},
+                                           {'y', 'Y'},
+                                           {'u', 'U'},
+                                           {'i', 'I'},
+                                           {'o', 'O'},
+                                           {'p', 'P'},
+                                           {'[', '{'},
+                                           {']', '}'},
+                                           {'\\', '|'}},
+                                          {{KEY_FN, KEY_FN},
+                                           {KEY_LEFT_SHIFT, KEY_LEFT_SHIFT},
+                                           {'a', 'A'},
+                                           {'s', 'S'},
+                                           {'d', 'D'},
+                                           {'f', 'F'},
+                                           {'g', 'G'},
+                                           {'h', 'H'},
+                                           {'j', 'J'},
+                                           {'k', 'K'},
+                                           {'l', 'L'},
+                                           {';', ':'},
+                                           {'\'', '\"'},
+                                           {KEY_ENTER, KEY_ENTER}},
+                                          {{KEY_LEFT_CTRL, KEY_LEFT_CTRL},
+                                           {KEY_OPT, KEY_OPT},
+                                           {KEY_LEFT_ALT, KEY_LEFT_ALT},
+                                           {'z', 'Z'},
+                                           {'x', 'X'},
+                                           {'c', 'C'},
+                                           {'v', 'V'},
+                                           {'b', 'B'},
+                                           {'n', 'N'},
+                                           {'m', 'M'},
+                                           {',', '<'},
+                                           {'.', '>'},
+                                           {'/', '?'},
+                                           {' ', ' '}}};
+
+class Keyboard_Class
+{
+public:
+    struct KeysState
     {
-        uint8_t value;
-        uint8_t x_1;
-        uint8_t x_2;
+        bool tab = false;
+        bool fn = false;
+        bool shift = false;
+        bool ctrl = false;
+        bool opt = false;
+        bool alt = false;
+        bool del = false;
+        bool enter = false;
+        bool space = false;
+        uint8_t modifiers = 0;
+
+        std::vector<char> word;
+        std::vector<uint8_t> hid_keys;
+        std::vector<uint8_t> modifier_keys;
+
+        void reset()
+        {
+            tab = false;
+            fn = false;
+            shift = false;
+            ctrl = false;
+            opt = false;
+            alt = false;
+            del = false;
+            enter = false;
+            space = false;
+            modifiers = 0;
+            word.clear();
+            hid_keys.clear();
+            modifier_keys.clear();
+        }
     };
 
-    struct Point2D_t 
+private:
+    std::vector<Point2D_t> _key_list_buffer;
+    std::vector<Point2D_t> _key_pos_print_keys; // only text: eg A,B,C
+    std::vector<Point2D_t> _key_pos_hid_keys;   // print key + space, enter, del
+    std::vector<Point2D_t>
+        _key_pos_modifier_keys; // modifier key: eg shift, ctrl, alt
+    KeysState _keys_state_buffer;
+    bool _is_caps_locked;
+    uint8_t _last_key_size;
+
+    void _set_output(const std::vector<int> &pinList, uint8_t output);
+    uint8_t _get_input(const std::vector<int> &pinList);
+
+public:
+    Keyboard_Class() : _is_caps_locked(false)
     {
-        int x;
-        int y;
-    };
+    }
 
-    const std::vector<int> output_list = {8, 9, 11};
-    const std::vector<int> input_list = {13, 15, 3, 4, 5, 6, 7};
-    // const std::vector<int> input_list = {1, 2, 3, 4, 5, 6, 7};
+    void begin();
+    uint8_t getKey(Point2D_t keyCoor);
 
-    const Chart_t X_map_chart[7] = 
+    void updateKeyList();
+
+    inline std::vector<Point2D_t> &keyList()
     {
-        {1, 0, 1},
-        {2, 2, 3},
-        {4, 4, 5},
-        {8, 6, 7},
-        {16, 8, 9},
-        {32, 10, 11},
-        {64, 12, 13}
-    };
+        return _key_list_buffer;
+    }
 
-    struct KeyValue_t 
+    inline KeyValue_t getKeyValue(const Point2D_t &keyCoor)
     {
-        const char* value_first;
-        const int value_num_first;
-        const char* value_second;
-        const int value_num_second;
-    };
+        return _key_value_map[keyCoor.y][keyCoor.x];
+    }
 
-    const KeyValue_t _key_value_map[4][14] = {
-    {{"`", KEY_GRAVE, "~", KEY_GRAVE},
-     {"1", KEY_1, "!", KEY_1},
-     {"2", KEY_2, "@", KEY_2},
-     {"3", KEY_3, "#", KEY_3},
-     {"4", KEY_4, "$", KEY_4},
-     {"5", KEY_5, "%", KEY_5},
-     {"6", KEY_6, "^", KEY_6},
-     {"7", KEY_7, "&", KEY_7},
-     {"8", KEY_8, "*", KEY_KPASTERISK},
-     {"9", KEY_9, "(", KEY_KPLEFTPAREN},
-     {"0", KEY_0, ")", KEY_KPRIGHTPAREN},
-     {"-", KEY_MINUS, "_", KEY_KPMINUS},
-     {"=", KEY_EQUAL, "+", KEY_KPPLUS},
-     {"del", KEY_BACKSPACE, "del", KEY_BACKSPACE}},
-    {{"tab", KEY_TAB, "tab", KEY_TAB},
-     {"q", KEY_Q, "Q", KEY_Q},
-     {"w", KEY_W, "W", KEY_W},
-     {"e", KEY_E, "E", KEY_E},
-     {"r", KEY_R, "R", KEY_R},
-     {"t", KEY_T, "T", KEY_T},
-     {"y", KEY_Y, "Y", KEY_Y},
-     {"u", KEY_U, "U", KEY_U},
-     {"i", KEY_I, "I", KEY_I},
-     {"o", KEY_O, "O", KEY_O},
-     {"p", KEY_P, "P", KEY_P},
-     {"[", KEY_LEFTBRACE, "{", KEY_LEFTBRACE},
-     {"]", KEY_RIGHTBRACE, "}", KEY_RIGHTBRACE},
-     {"\\", KEY_BACKSLASH, "|", KEY_BACKSLASH}},
-    {{"fn", 0, "fn", 0},
-     {"shift", 0, "shift", 0},
-     {"a", KEY_A, "A", KEY_A},
-     {"s", KEY_S, "S", KEY_S},
-     {"d", KEY_D, "D", KEY_D},
-     {"f", KEY_F, "F", KEY_F},
-     {"g", KEY_G, "G", KEY_G},
-     {"h", KEY_H, "H", KEY_H},
-     {"j", KEY_J, "J", KEY_J},
-     {"k", KEY_K, "K", KEY_K},
-     {"l", KEY_L, "L", KEY_L},
-     {";", KEY_SEMICOLON, ":", KEY_SEMICOLON},
-     {"'", KEY_APOSTROPHE, "\"", KEY_APOSTROPHE},
-     {"enter", KEY_ENTER, "enter", KEY_ENTER}},
-    {{"ctrl", KEY_LEFTCTRL, "ctrl", KEY_LEFTCTRL},
-     {"opt", 0, "opt", 0},
-     {"alt", KEY_LEFTALT, "alt", KEY_LEFTALT},
-     {"z", KEY_Z, "Z", KEY_Z},
-     {"x", KEY_X, "X", KEY_X},
-     {"c", KEY_C, "C", KEY_C},
-     {"v", KEY_V, "V", KEY_V},
-     {"b", KEY_B, "B", KEY_B},
-     {"n", KEY_N, "N", KEY_N},
-     {"m", KEY_M, "M", KEY_M},
-     {",", KEY_COMMA, "<", KEY_COMMA},
-     {".", KEY_DOT, ">", KEY_DOT},
-     {"/", KEY_KPSLASH, "?", KEY_KPSLASH},
-     {"space", KEY_SPACE, "space", KEY_SPACE}}};
+    uint8_t isPressed();
+    bool isChange();
+    bool isKeyPressed(char c);
 
-    class Keyboard
+    void updateKeysState();
+
+    void update()
     {
-        public:
-            
-            struct KeysState
-            {
-                bool tab = false;
-                bool fn = false;
-                bool shift = false;
-                bool ctrl = false;
-                bool opt = false;
-                bool alt = false;
-                bool del = false;
-                bool enter = false;
-                bool space  = false;
+        updateKeyList();
+        updateKeysState();
+    }
+    inline KeysState &keysState()
+    {
+        return _keys_state_buffer;
+    }
 
-                std::vector<char> values;
-                std::vector<int> hidKey;
-
-                void reset()
-                {
-                    tab = false;
-                    fn = false;
-                    shift = false;
-                    ctrl = false;
-                    opt = false;
-                    alt = false;
-                    del = false;
-                    enter = false;
-                    space = false;
-
-                    values.clear();
-                    hidKey.clear();
-                }
-            };
-
-
-        private:
-            std::vector<Point2D_t> _key_list_buffer;
-
-            std::vector<Point2D_t> _key_values_without_special_keys;
-            KeysState _keys_state_buffer;
-
-            bool _is_caps_locked;
-            uint8_t _last_key_size;
-
-
-            void _set_output(const std::vector<int>& pinList, uint8_t output);
-            uint8_t _get_input(const std::vector<int>& pinList);
-
-
-        public:
-            Keyboard() : 
-                _is_caps_locked(false),
-                _last_key_size(0)
-                {}
-
-            void init();
-
-            Point2D_t getKey();
-
-            uint8_t getKeyNum(Point2D_t keyCoor);
-
-            void updateKeyList();
-            inline std::vector<KEYBOARD::Point2D_t>& keyList() { return _key_list_buffer; }
-
-            inline KeyValue_t getKeyValue(const Point2D_t& keyCoor) { return _key_value_map[keyCoor.y][keyCoor.x]; }
-
-            bool isKeyPressing(int keyNum);
-
-            void updateKeysState();
-            inline KeysState& keysState() { return _keys_state_buffer; }
-
-            inline bool capslocked(void) { return _is_caps_locked; }
-            inline void setCapsLocked(bool isLocked) { _is_caps_locked = isLocked; }
-
-            bool isChanged();
-            inline uint8_t isPressed() { return _key_list_buffer.size(); }
-    };
-}
+    inline bool capslocked(void)
+    {
+        return _is_caps_locked;
+    }
+    inline void setCapsLocked(bool isLocked)
+    {
+        _is_caps_locked = isLocked;
+    }
+};
+#endif
